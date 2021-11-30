4a2e462d63963ce0df4b0905032b73ebd8f90272
Maral Ashyrova <ashirova310197@gmail.com>
2021-12-01 01:11:22 +0200

    optimize waiting time

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def main():
     with Loader() as loader:
         loader.store_web_page_content()
         loader.export_table_data()
-        loader.extract_data_from_pdf()
 
 
 if __name__ == '__main__':
